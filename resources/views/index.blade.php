e101b1297990ed0236f82c53f32c186a5f60a3c4
Shadyar Bzhar Othman <shadyarbzhar22@gmail.com>
2023-12-21 18:07:58 +0300

    add tailwind css and images

---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1 class="text-red-800">Hello World</h1>
+</x-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        // It will include all file and subdirectory files in resources directory
+        "./resources/**/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
