660a0c96630fe0e7f875b6832dfd944e065829a5
Shadyar Bzhar Othman <shadyarbzhar22@gmail.com>
2023-12-27 00:33:13 +0300

    add dashboard page functionality

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -14,6 +16,23 @@ class PageController extends Controller
 
     public function dashboard()
     {
-        return view("dashboard.index");
+        $products = Product::limit(8)->latest()->get();
+        $product = count(Product::all());
+        $user = count(User::all());
+        $order = count(Order::all());
+        $orderpending = count(Order::where("state_id", 1)->get());
+        $orderaccepted = count(Order::where("state_id", 2)->get());
+        $orderfinished = count(Order::where("state_id", 3)->get());
+
+
+        return view("dashboard.index", [
+            "products" => Product::limit(8)->latest()->get(),
+            "product" => count(Product::all()),
+            "user" => count(User::all()),
+            "order" => count(Order::all()),
+            "orderpending" => count(Order::where("state_id", 1)->get()),
+            "orderaccepted" => count(Order::where("state_id", 2)->get()),
+            "orderfinished" => count(Order::where("state_id", 3)->get()),
+        ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,3 +1,44 @@
 <x-layouts.dashboard>
-    <p class="text-center">Dashboard</p>
+    <div class="space-y-2">
+        <h1>Dashboard</h1>
+        <div class="flex-col sm:flex-row flex justify-start items-center space-x-2 space-y-2">
+            <div class="flex flex-col justify-center items-start p-4 bg-white border border-gray-200 rounded-lg shadow">
+                <div class="flex justify-start items-center space-x-2 mb-2">
+                    <h5 class="mb-2 text-base font-bold">Product Number: </h5>
+                    <h1 class="mb-2 text-sm font-bold text-slate-400">{{ $product }}</h1>
+                </div>
+                <div class="flex justify-start items-center space-x-2 mb-2">
+                    <h5 class="mb-2 text-base font-bold">User Number: </h5>
+                    <h1 class="mb-2 text-sm font-bold text-slate-400">{{ $user }}</h1>
+                </div>
+                <div class="flex justify-start items-center space-x-2 mb-2">
+                    <h5 class="mb-2 text-base font-bold">Order Number: </h5>
+                    <h1 class="mb-2 text-sm font-bold text-slate-400">{{ $order }}</h1>
+                </div>
+            </div>
+            <div class="flex flex-col justify-center items-start p-4 bg-white border border-gray-200 rounded-lg shadow">
+                <div class="flex justify-start items-center space-x-2 mb-2">
+                    <h5 class="mb-2 text-base font-bold">Pending Order Number: </h5>
+                    <h1 class="mb-2 text-sm font-bold text-slate-400">{{ $orderpending }}</h1>
+                </div>
+                <div class="flex justify-start items-center space-x-2 mb-2">
+                    <h5 class="mb-2 text-base font-bold">Accepted Order Number: </h5>
+                    <h1 class="mb-2 text-sm font-bold text-slate-400">{{ $orderaccepted }}</h1>
+                </div>
+                <div class="flex justify-start items-center space-x-2 mb-2">
+                    <h5 class="mb-2 text-base font-bold">Finished Number: </h5>
+                    <h1 class="mb-2 text-sm font-bold text-slate-400">{{ $orderfinished }}</h1>
+                </div>
+
+            </div>
+        </div>
+        <div>
+            <h1>Newest Product</h1>
+            <div class="flex justify-start items-center flex-wrap gap-2">
+                @foreach ($products as $product)
+                    <x-cards.product :product="$product" />
+                @endforeach
+            </div>
+        </div>
+    </div>
 </x-layouts.dashboard>
